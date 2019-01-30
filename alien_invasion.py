1cd8b158e2f45317d0bc28db1835e8de3bbaf45c
Maxi Merrillees <maxi.merrillees@gmail.com>
2019-01-30 14:05:43 +1100

    Limit the ship's range

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,14 +10,18 @@ def run_game():
   settings = Settings()
   pygame.display.set_caption("Alien Invasion")
   screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
-  ship = Ship(screen)
+  ship = Ship(screen, settings)
 
   # Start the main loop for the game.
   while True:
     # Check events from game_functions module
-    check_events()
+    check_events(ship)
+
+    # Update the location of the ship
+    ship.update()
 
     # Redraw the screen during each pass through the loop.
+    
     update_screen(settings, screen, ship)
 
     # Make the most recently drawn screen visible.
--- game_functions.py
+++ game_functions.py
@@ -1,13 +1,34 @@
 import sys
 import pygame
 
-def check_events():
+def check_events(ship):
   """ Respond to keypresses and mouse events. """
   
   # Watch for keybaord and mouse events.
   for event in pygame.event.get():
+    
+    # Close the screen when the X button is pressed
     if event.type == pygame.QUIT:
       sys.exit()
+    
+    # Move left and right when the left and right arrow keys are pressed
+    elif event.type == pygame.KEYDOWN:
+      # Move the  ship to the right
+      if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+      
+      # Move the ship to the left
+      elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+    elif event.type == pygame.KEYUP:
+      # Stop movement to the right
+      if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+      
+      # Stop movement to the right
+      elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
   
 # Redraw the screen during each pass through the loop.
 def update_screen(settings, screen, ship):
